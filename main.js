cc1c5cfecfc3b5b59261c43955494438f207df5b
filipposfilippidis <162036413+filipposfilippidis@users.noreply.github.com>
2024-04-14 08:06:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Ariadni";
+var last_name = "Filippidou";
+console.log(last_name);
+
+first_name.concat(last_name);
+
+var a = 10;
+var b = 5;
+
+console.log()
+f_name.concat(l_name)
+
+var add = a + b ; 
+console.log(add);
